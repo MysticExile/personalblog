f991d05499ea322e9251f8ffcf298900d2b52fbd
MysticExile <ethan.baboeram@gmail.com>
2022-04-09 01:16:04 +0200

    Fix the icon change not working && clean up the code.

--- js/main.js
+++ js/main.js
@@ -3,36 +3,41 @@ window.addEventListener('load', init);
 let nav;
 
 function init() {
-    //Retrieve gallery and add a click event for every Pokémon
+    // Retrieve navigation bar and add a click event for the darkmode button
     nav = document.getElementById('nav');
     nav.addEventListener('click', darkmodeClickHandler);
 }
 
 function darkmodeClickHandler(e) {
+    // store the target into a variable
     let clickedItem = e.target;
-    //console.log(clickedItem);
-    //console.log(clickedItem.nodeName);
     //Check if we clicked on a button
     if (clickedItem.nodeName !== 'BUTTON' && clickedItem.nodeName !== 'I') {
         //console.log("true 1");
         return;
     }
-    if (clickedItem.getAttribute("id") !== "button-darkmode" && clickedItem.getAttribute("class") !== "button-darkmode"){
+    if (clickedItem.getAttribute("id") !== "button-darkmode" && clickedItem.getAttribute("class") !== "button-darkmode") {
         //console.log("true 2");
         return;
     }
-    let dark = document.getElementsByClassName("button-darkmode");
+    // get the button element containing the darkmode icons
+    let buttonElement = document.getElementById("moon");
+    // get the element containing the link to the stylesheets
     const theme = document.querySelector("#theme");
     if (theme.getAttribute("href") === "css/style.css") {
         // ... then switch it to "dark-theme.css"
         theme.href = "css/dark.css";
-        dark.innerHTML = '';
-        dark.innerHTML = '<i class="fa-solid fa-moon" id="button-darkmode"></i>';
+        // clear the previous icon
+        buttonElement.innerHTML = '';
+        // insert the new icon
+        buttonElement.innerHTML = '<i class="fa-solid fa-moon" id="button-darkmode"></i>';
         // Otherwise...
     } else {
         // ... switch it to "light-theme.css"
         theme.href = "css/style.css";
-        dark.innerHTML = '';
-        dark.innerHTML = '<i class="fa-solid fa-sun" id="button-darkmode"></i>';
+        // clear previous icon
+        buttonElement.innerHTML = '';
+        // insert the new icon
+        buttonElement.innerHTML = '<i class="fa-solid fa-sun" id="button-darkmode"></i>';
     }
 }
