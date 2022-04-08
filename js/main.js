6785fd4f772b9f2867de359eff2051bf76234418
MysticExile <ethan.baboeram@gmail.com>
2022-04-08 20:33:57 +0200

    fix darkmode buttom hopefully

--- js/main.js
+++ js/main.js
@@ -10,23 +10,29 @@ function init() {
 
 function darkmodeClickHandler(e) {
     let clickedItem = e.target;
-    console.log(clickedItem);
+    //console.log(clickedItem);
+    //console.log(clickedItem.nodeName);
     //Check if we clicked on a button
-    if (clickedItem.nodeName !== 'BUTTON' && clickedItem.getAttribute("id") !== "dark") {
+    if (clickedItem.nodeName !== 'BUTTON' && clickedItem.nodeName !== 'I') {
+        //console.log("true 1");
         return;
     }
-    clickedItem.innerHTML = '';
+    if (clickedItem.getAttribute("id") !== "button-darkmode" && clickedItem.getAttribute("class") !== "button-darkmode"){
+        //console.log("true 2");
+        return;
+    }
+    let dark = document.getElementsByClassName("button-darkmode");
     const theme = document.querySelector("#theme");
     if (theme.getAttribute("href") === "css/style.css") {
         // ... then switch it to "dark-theme.css"
         theme.href = "css/dark.css";
-        clickedItem.innerHTML = '';
-        clickedItem.innerHTML = '<i class="fa-solid fa-moon" id="icon"></i>';
+        dark.innerHTML = '';
+        dark.innerHTML = '<i class="fa-solid fa-moon" id="button-darkmode"></i>';
         // Otherwise...
     } else {
         // ... switch it to "light-theme.css"
         theme.href = "css/style.css";
-        clickedItem.innerHTML = '';
-        clickedItem.innerHTML = '<i class="fa-solid fa-sun" id="icon"></i>';
+        dark.innerHTML = '';
+        dark.innerHTML = '<i class="fa-solid fa-sun" id="button-darkmode"></i>';
     }
 }
