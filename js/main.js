78a1ea670b3521141c18ec3aa9d883ace0c33e1f
MysticExile <ethan.baboeram@gmail.com>
2022-04-08 17:53:11 +0200

    bug fixes

--- js/main.js
+++ js/main.js
@@ -10,22 +10,23 @@ function init() {
 
 function darkmodeClickHandler(e) {
     let clickedItem = e.target;
+    console.log(clickedItem);
     //Check if we clicked on a button
-    /*if (clickedItem.nodeName !== 'BUTTON') {
+    if (clickedItem.nodeName !== 'BUTTON' && (clickedItem.getAttribute("id") !== "dark" || clickedItem.getAttribute("class") !== "icon")) {
         return;
-    }*/
+    }
     clickedItem.innerHTML = '';
     const theme = document.querySelector("#theme");
     if (theme.getAttribute("href") === "css/style.css") {
         // ... then switch it to "dark-theme.css"
         theme.href = "css/dark.css";
         clickedItem.innerHTML = '';
-        clickedItem.innerHTML = '<i class="fa-solid fa-moon"></i>';
+        clickedItem.innerHTML = '<i class="fa-solid fa-moon" id="icon"></i>';
         // Otherwise...
     } else {
         // ... switch it to "light-theme.css"
         theme.href = "css/style.css";
         clickedItem.innerHTML = '';
-        clickedItem.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        clickedItem.innerHTML = '<i class="fa-solid fa-sun" id="icon"></i>';
     }
 }
